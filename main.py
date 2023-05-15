e9deb4574d1478542e468a902020608439a570c8
OsmanErunali <osmanerunali@gmail.com>
2023-05-15 22:00:25 +0300

    This function handles the task of receiving a single data point from the sensor and storing it.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+data_list = []
 
+
+def collect_sensor_data():
+    # This function handles the task of receiving a single data point from the sensor and storing it.
+    while True:
+        data = input("Data from sensor (or 'q' to quit): ")
+        if data.lower() == 'q':
+            break
+        data = int(data)
+        data_list.append(data)
+        if len(data_list) > 20:
+            data_list.pop(0)
+    return data_list
 
 
